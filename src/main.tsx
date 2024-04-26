345fed93b1d2b5c0a59c51a57ed261469088568a
Mohamed Khachira <mohamed.khachira@gmail.com>
2024-04-26 11:27:03 +0200

    Fix vite base URL

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Suspense fallback={<div>Loading ...</div>}>
-			<BrowserRouter>
+			<BrowserRouter basename={process.env.PUBLIC_URL}>
 				<App />
 			</BrowserRouter>
 		</Suspense>
