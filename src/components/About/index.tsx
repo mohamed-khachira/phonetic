20c0e81133f539e767b2e00cf44c27e41ff6ba94
Mohamed Khachira <mohamed.khachira@gmail.com>
2024-05-25 23:46:38 +0200

    add our team section

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,11 +1,14 @@
 import { CheckmarkCircleOutline } from "react-ionicons";
 import education from "../../assets/1549325147.svg";
 
+import ameni from "../../assets/ameni.jpeg";
+import eya from "../../assets/eya.jpeg";
+
 const About = () => {
 	return (
 		<div id="about" className="w-full md:px-[200px] px-8 flex md:flex-row flex-col items-center justify-between py-10 md:gap-0 gap-16 bg-[#ebf8ff]">
 			<div
-				className="md:w-[700px] md:h-[700px] w-[350px] h-[350px] bg-no-repeat bg-cover relative plane-mask"
+				className="md:w-[700px]  w-[350px] h-[350px] bg-no-repeat bg-cover relative plane-mask"
 				style={{ backgroundImage: `url(${education})` }}
 			/>
 			<div className="flex flex-col md:w-[48%] w-full md:px-0 px-8">
@@ -39,6 +42,31 @@ const About = () => {
 					</div>
 				</div>
 			</div>
+			<div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8" style={{width: '500px'}}>
+            <div className="mb-12">
+                <h2 className="font-manrope text-5xl text-center font-bold text-gray-900 ">Notre équipe</h2>
+            </div>
+				<div className="grid grid-cols-1 min-[500px]:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 gap-8 max-w-xl mx-auto md:max-w-3xl lg:max-w-full">
+						<div className="block group md:col-span-2 lg:col-span-1 ">
+							<div className="relative mb-6">
+								<img src={ameni} alt="Ameni khchira"
+									className="w-40 h-40 rounded-full mx-auto transition-all duration-500 object-cover border border-solid border-transparent group-hover:border-indigo-600" />
+							</div>
+							<h4
+								className="text-xl font-semibold text-gray-900 mb-2 capitalize text-center transition-all duration-500 group-hover:text-indigo-600">
+								Amani Khchira</h4>
+						</div>
+						<div className="block group md:col-span-2 lg:col-span-1 ">
+							<div className="relative mb-6">
+								<img src={eya} alt="Eya Larayedh"
+									className="w-40 h-40 rounded-full mx-auto transition-all duration-500 object-cover border border-solid border-transparent group-hover:border-indigo-600" />
+							</div>
+							<h4
+								className="text-xl font-semibold text-gray-900 mb-2 capitalize text-center transition-all duration-500 group-hover:text-indigo-600">
+								Eya Larayedh</h4>
+						</div>
+				</div>
+        	</div>
 		</div>
 	);
 };
